575ad7dddc871b4241a3dfd166d960f1cb5b0f4f
Meg Galvez <meg@oakmaine.com>
2019-07-08 17:19:15 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,2 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migrations[5.2]
+  def change 
+    add_column :students, :grade, :integer 
+    add_column :students, :birthdate, :string
+  
+  end 
 end 
